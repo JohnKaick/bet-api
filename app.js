e67d68cda5e6fbbcb6bec3348f53c8fabd904154
JohnKaick <john.kaick@gmail.com>
2018-11-20 22:30:46 -0200

    publish website umbler

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const fs = require('fs');
-require('dotenv').config()
 
 const app = express();
 const serverDir = path.resolve(__dirname, 'server');
